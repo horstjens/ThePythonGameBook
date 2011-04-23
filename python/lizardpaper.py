015b3889ec782f568a9319cd15c71db47ba198ca
Horst JENS <horstjens@gmail.com>
2011-04-23 08:06:28 +0200

    start menu to choose between classic and lizard mode. better names for variables.

--- python/lizardpaper.py
+++ python/lizardpaper.py
@@ -3,6 +3,10 @@
 #
 #       lizardpaper.py 
 #       Copyright 2011 Horst JENS <horst.jens@spielend-programmieren.at>
+#       This program is part of ThePythonGameBook , 
+#       see http://ThePythonGameBook.com for more information.
+#       Check the newest version of this file:
+#       https://github.com/horstjens/ThePythonGameBook/blob/master/python/lizardpaper.py
 #       
 #       This program is free software; you can redistribute it and/or modify
 #       it under the terms of the GNU General Public License as published by
@@ -24,45 +28,61 @@
 #       
 #       MA 02110-1301, USA.
 
-def main():
-    """rock paper scissor lizard spock"""
-    # things beat things, equal things are a draw
-    import random
-    # key and meaning of this key
-    keys = {"r":"rock", 
-            "s":"scissors",
-            "p":"paper",
-            "l":"lizard",
-            "o":"Spock"
-            }
+def startmenu():
+    print "*** Welcome !***"
+    print "please choose between a classic game of 'rock, paper, scissors'"
+    print "or a game of the newer variant 'rock, paper, scissors, lizard, Spock' "
+    print 
+    print "see Wikipedia for game rules and more information:"
+    print "http://en.wikipedia.org/wiki/Rock_paper_scissors"
+    print "http://en.wikipedia.org/wiki/Rock-paper-scissors-lizard-spock"
+    print
+    mode = "x"
+    while mode != "c" and mode != "n": 
+        print "please press either c for classic or n for newer variant"
+        print "and press ENTER to start"
+        mode = raw_input("Your choice: \n>")
+    if mode == "c":
+        game("classic")
+    elif mode == "n":
+        game("new")
+    else:
+        return "game variant error"
 
-    #  victor victim victorytext
+def game(mode="classic"):
+    """rock paper scissor lizard spock
+    mode can be new or classic"""
+    import random
+    if mode != "new" and mode != "classic":
+        return "unknow parameter. please start game with either 'classic' or 'new'"
+    # { key: human-readable key description }
+    things = {"r":"rock", 
+            "s":"scissors",
+            "p":"paper"}
+    if mode == "new": # expand only for new version
+        things["l"] = "lizard"
+        things["o"] = "Spock"
+        
+    # { victor : { victim : (victorytext loosertext) }}
     wintext = {
-              "r": { "s": "rock crushes scissors",
-                     "l": "rock crushes lizard"},
-              "s": { "p": "scissors cut paper",
-                     "l": "scissors decapitate lizard"},
-              "p": { "o": "paper disproves Spock",
-                     "r": "paper covers rock" },
-              "o": { "s": "Spock smashes scissors",
-                     "r": "Spock vaporizes rock" },
-              "l": { "p": "lizard eats paper",
-                     "o": "lizard poisons spock" } 
+              "r": { "s": ("rock crushes scissors", "scissors is crushed by rock"),
+                     "l": ("rock crushes lizard", "lizard is crushed by rock")},
+              "s": { "p": ("scissors cut paper", "paper is cut by scissors"),
+                     "l": ("scissors decapitate lizard", "lizard is decapitated by scissors")},
+              "p": { "o": ("paper disproves Spock", "Spock is disproved by paper"),
+                     "r": ("paper covers rock", "rock is enveloped by paper") },
+              "o": { "s": ("Spock smashes scissors", "scissors are smashed by Spock"),
+                     "r": ("Spock vaporizes rock", "rock is vaporized by Spock") },
+              "l": { "p": ("lizard eats paper", "paper is eaten by lizard"),
+                     "o": ("lizard poisons Spock", "Spock is poisoned by lizard") } 
               }
     import random
     question = "What do you play ? \n"
-    validstring = ""
-    for thingy in wintext.keys():
-        question += thingy + ": " + keys[thingy] + "\n"
-        validstring += thingy
-    validstring += "q" # add "q" to quit
-    question += "\n please press one of the keys listed above and ENTER \n or press q and ENTER to quit \n>" 
+    for thingy in things.keys():
+        question += thingy + ": " + things[thingy] + "\n"
+    question += "\n please press one of the keys listed above and ENTER \n" 
     mainloop = True
-    #playerpoints = 0
-    #computerpoints = 0
     rounds = 0
-    print "*** Welcome to a game of rock, paper, scissors, lizard, Spock ! ***"
-    print " see http://en.wikipedia.org/wiki/Rock-paper-scissors-lizard-spock\n for game rules"
     # ------ generating players --------
     players = {} # name, nature, thing, points
     answer = ""
@@ -90,51 +110,42 @@ def main():
         for player in players.keys():
             if players[player]["nature"] == "h": # human                
                 playerthing = ""
-                while not (playerthing in validstring) or playerthing == "" or playerthing == None:
+                while not (playerthing in things.keys()) or playerthing == "" or playerthing == None:
                     print "******** player %s, it is your turn ! *******" % player
                     playerthing = raw_input(question)
-                if playerthing == "q":
-                    print "i'm sad that you want to quit the game"
-                    mainloop = False
-                    break # exit loop
-                else:
-                    players[player]["thing"] = playerthing # adding answer to dict
+                players[player]["thing"] = playerthing # adding answer to dict
             else: # computer player
-                players[player]["thing"] = random.choice(wintext.keys()) # computerthing
+                players[player]["thing"] = random.choice(things.keys()) # computerthing
         rounds += 1
         
-        # --- output ----
-        for player in players.keys():
+        for player in players.keys(): # --- output ----
             playerthing = players[player]["thing"]
             print "The coice of player %s is: %s " % (player, playerthing)
-        #print "The computer choice is %s " % keys[computerthing]
-        # ---- calculate winner
-        for player in players.keys():
-            print "calculating points for player %s" % player
+        for player in players.keys():         # ---- calculate winner
+            print "...calculating points for player %s..." % player
             playerthing = players[player]["thing"]
             for enemy in players.keys():
                 if player != enemy:
                     enemything = players[enemy]["thing"]
-                    print "%s of %s versus %s of %s" % (keys[playerthing], player, keys[enemything], enemy)                        
+                    print "%s of %s versus %s of %s" % (things[playerthing], player, things[enemything], enemy)                        
                     if playerthing == enemything:
                         print "this is a draw (0 points)"
                     elif enemything in wintext[playerthing].keys():
                         players[player]["points"] += 1
-                        print wintext[playerthing][enemything],"(+1 point)"
+                        print wintext[playerthing][enemything][0],"(+1 point)" # victory
                     else:
-                        print wintext[enemything][playerthing],"(0 points)"
-        #------ summary -------
-        print "====== result ======="
+                        print wintext[enemything][playerthing][1],"(0 points)" # loose
+        print "====== result (round %i) =======" % rounds     #------ summary -------
         for player in players.keys():
             print "points: %i for %s" %( players[player]["points"], player)
         print "------ next round -------"
-        delay = raw_input("press ENTER to continue, q and ENTER to quit")
-        if delay == "q":
+        playMore = raw_input("press ENTER to continue, q and ENTER to quit")
+        if playMore == "q":
             mainloop = False
         print "\n\n\n" # three empty lines
     print "bye-bye"
     return 0 # (computerpoints, playerpoints)
 
 if __name__ == '__main__':
-    main()
+    startmenu()
 
