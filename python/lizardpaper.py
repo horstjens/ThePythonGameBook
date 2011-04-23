60c970545d5536d0271d79445a6e2c772f59a1f7
Horst JENS <horstjens@gmail.com>
2011-04-23 07:13:56 +0200

    lizardpaper

--- python/lizardpaper.py
+++ python/lizardpaper.py
@@ -58,40 +58,79 @@ def main():
     validstring += "q" # add "q" to quit
     question += "\n please press one of the keys listed above and ENTER \n or press q and ENTER to quit \n>" 
     mainloop = True
-    playerpoints = 0
-    computerpoints = 0
+    #playerpoints = 0
+    #computerpoints = 0
     rounds = 0
-    print " see http://en.wikipedia.org/wiki/Rock-paper-scissors-lizard-spock for game rules"
-    while mainloop:
-        playerthing = "xxxx"
-        print " ---- round played: %i ----- " % rounds
-        while not playerthing in validstring:
-            playerthing = raw_input(question)
-        if playerthing == "q":
-            print "i'm sad that you want to quit the game"
-            mainloop = False
-            break # exit loop
+    print "*** Welcome to a game of rock, paper, scissors, lizard, Spock ! ***"
+    print " see http://en.wikipedia.org/wiki/Rock-paper-scissors-lizard-spock\n for game rules"
+    # ------ generating players --------
+    players = {} # name, nature, thing, points
+    answer = ""
+    while answer == "":
+        print "At the moment, this game has %i players. Minimum to start a game is 2 players." % len(players)
+        if len(players.keys()) > 0:
+            print "-- list of players in the game --"
+            for player in players.keys():
+                print "name: %s  type: %s" % (player, players[player]["nature"])
+        print "----"
+        playername = raw_input("please type in the name of a new player and press ENTER \n"
+                               "press only ENTER to start the game \n>")
+        if playername == "":
+            break # exit
+        natureOfPlayer = "x"
+        while (natureOfPlayer not in "hc") or (natureOfPlayer == "") or (natureOfPlayer == None):
+            natureOfPlayer = raw_input("is this player a (h)uman or a (c)omputer ? press h or c and ENTER")
+        players[playername] = {"nature": natureOfPlayer , "thing": "xxx", "points":0 }# add player do dictionary
+    if len(players) < 2:
+        print "you need at least 2 players to start a game. Bye !"
+        return "too few players"
+        
+    while mainloop: # ----------- the game loop ------------    
+        print " ---- rounds played: %i ----- " % rounds
+        for player in players.keys():
+            if players[player]["nature"] == "h": # human                
+                playerthing = ""
+                while not (playerthing in validstring) or playerthing == "" or playerthing == None:
+                    print "******** player %s, it is your turn ! *******" % player
+                    playerthing = raw_input(question)
+                if playerthing == "q":
+                    print "i'm sad that you want to quit the game"
+                    mainloop = False
+                    break # exit loop
+                else:
+                    players[player]["thing"] = playerthing # adding answer to dict
+            else: # computer player
+                players[player]["thing"] = random.choice(wintext.keys()) # computerthing
         rounds += 1
-        computerthing = random.choice(wintext.keys())
+        
         # --- output ----
-        print "Your choice is: %s " % keys[playerthing]
-        print "The computer choice is %s " % keys[computerthing]
+        for player in players.keys():
+            playerthing = players[player]["thing"]
+            print "The coice of player %s is: %s " % (player, playerthing)
+        #print "The computer choice is %s " % keys[computerthing]
         # ---- calculate winner
-        if playerthing == computerthing:
-            print "this is a draw"
-        elif computerthing in wintext[playerthing].keys():
-            playerpoints += 1
-            print wintext[playerthing][computerthing]
-            print "YOU win !"
-        else:
-            computerpoints += 1
-            print wintext[computerthing][playerthing]
-            print "You loose....."
-        print "-------------------------"
-        print "points for computer: %i" % computerpoints
-        print "points for human player: %i" % playerpoints
+        for player in players.keys():
+            print "calculating points for player %s" % player
+            playerthing = players[player]["thing"]
+            for enemy in players.keys():
+                if player != enemy:
+                    enemything = players[enemy]["thing"]
+                    print "%s of %s versus %s of %s" % (keys[playerthing], player, keys[enemything], enemy)                        
+                    if playerthing == enemything:
+                        print "this is a draw (0 points)"
+                    elif enemything in wintext[playerthing].keys():
+                        players[player]["points"] += 1
+                        print wintext[playerthing][enemything],"(+1 point)"
+                    else:
+                        print wintext[enemything][playerthing],"(0 points)"
+        #------ summary -------
+        print "====== result ======="
+        for player in players.keys():
+            print "points: %i for %s" %( players[player]["points"], player)
         print "------ next round -------"
-        delay = raw_input("press ENTER to continue")
+        delay = raw_input("press ENTER to continue, q and ENTER to quit")
+        if delay == "q":
+            mainloop = False
         print "\n\n\n" # three empty lines
     print "bye-bye"
     return 0 # (computerpoints, playerpoints)
