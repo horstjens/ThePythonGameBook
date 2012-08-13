c30490c2ab34f57150e2576466e4845039f5fc73
HorstJENS <horstjens@gmail.com>
2012-08-13 17:12:05 +0200

    pycrawl walkdemo work in progress

--- python/pycrawl_walkdemo.py
+++ python/pycrawl_walkdemo.py
@@ -45,7 +45,7 @@ for y in list(range(18)):
         print(y,x,blocks[y,x])
 #print(blocks)
 
-tmpblock = blocks[(prow,pcol)]
+#tmpblock = blocks[(prow,pcol)]
 
 #create screenstring
 def calcscreen(paintplayer=True):
@@ -79,3 +79,64 @@ while moves < 10:
     
     print(screen)
     moves += 1
+    
+    
+#-------------yipyip-----------
+
+"""
+How to manage an ascii level map.
+
+Tested with Python 2.6, 3.2.
+"""
+
+
+raw_level =\
+"""\
+######
+#....#
+#....#
+######\
+"""
+
+class Level(object):
+
+    def __init__(self, level):
+        self.level_map = list(map(list, level.split()))
+
+
+    def __getitem__(self, xy):
+        x, y = xy
+        return self.level_map[y][x]
+
+
+    def __setitem__(self, xy, item):
+        x, y = xy
+        self.level_map[y][x] = item
+
+
+    def __iter__(self):
+        return ("".join(row) for row in self.level_map)
+
+
+    def __str__(self):
+        return "\n".join(row for row in self)
+
+
+def main():
+
+    print('___')
+    print(raw_level)
+    print( '~~~')
+
+    alevel = Level(raw_level)
+    print("alevel")
+    print(alevel)
+    print()
+    print("alevel rows and setitem")
+    alevel[1,2] = "?"
+    for row in alevel:
+        print(row)
+
+if __name__ == '__main__':
+    main()
+
