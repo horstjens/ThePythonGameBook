c70188f0c1eca81874f15b98c42c0524ecc1474a
Horst JENS <horstjens@gmail.com>
2013-07-13 22:38:53 +0200

    better comments

--- python/goblins/slowgoblins001.py
+++ python/goblins/slowgoblins001.py
@@ -1,5 +1,5 @@
-__author__ = 'Horst JENS'
-# see http://ThePythonGameBook.com
+# part of http://ThePythonGameBook.com
+# source code: https://github.com/horstjens/ThePythonGameBook/blob/master/python/goblins/slowgoblins001.py
 
 hitpointsStinky = 22
 hitpointsGrunty = 43
