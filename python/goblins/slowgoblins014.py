97d31935dfab150abf40f895ffc0181d64057cbf
wolperite <kakaoninja@gmx.at>
2013-07-31 11:56:17 +0200

    Create slowgoblins014.py

---
+++ python/goblins/slowgoblins014.py
@@ -0,0 +1,144 @@
+import random
+
+###
+def isMethod(a):
+    """testing if the attribute a of a class instance is a method.
+    returns True or False"""
+    #print(type(a))
+    if "method" in repr(a):
+        print("Methode drin")
+        return True
+    else:
+        print("no method")
+        return False
+
+###
+
+class Goblin(object):
+    """generic goblin with randomized stat values"""
+    counter = 0 # this is a class attribute
+
+
+    def __init__(self):
+        """creates a new goblin instance"""
+        self.attack = random.gauss(10, 2)    # float value
+        self.defense = random.gauss(10, 2)   # float value
+        self.hitpoints = random.gauss(20, 3) # float value
+        self.fullhealth = self.hitpoints # copy
+        
+        #statistics
+        self.damage_dealt = 0
+        self.damage_received = 0
+
+    def report(self):
+        """returns a string with the actual stats"""
+        text = "\natt: {} def: {}hp: {}".format(self.attack, self.defense, self.hitpoints)
+        text += "\ni have {0:3f}% of my hitpoints".format(self.hitpoints / self.fullhealth)
+        return text
+
+def sign(a, b):
+    """compares a with b and returns a "<","=" or ">" sign """
+    if a < b:
+        return "<"
+    elif a > b:
+        return ">"
+    else:
+        return "="
+
+def compareValues(a, b):
+    """returns a string with a table comparing the values of a and b"""
+    text =  "\n          Stinky | vs. | Grunty "
+    text += "\n ----------------+-----+-----------"
+    text += "\n hitpoints: {:>4.1f} |  {}  | {:>4.1f}".format(a.hitpoints, sign(a.hitpoints, b.hitpoints), b.hitpoints)
+    text += "\n attack:    {:>4.1f} |  {}  | {:>4.1f}".format(a.attack, sign(a.attack, b.attack), b.attack)
+    text += "\n defense:   {:>4.1f} |  {}  | {:>4.1f}".format(a.defense, sign(a.defense, b.defense), b.defense)
+    text += "\n"
+    return text
+
+def output(combatround, a, b):
+    """returns a string with combatround and both hitpoints form a and b"""
+    return "\n---combat round {0:3d}--- Stinky: {1:.0f} Grunty: {2:.0f}".format(combatround, a.hitpoints, b.hitpoints)
+
+def strike(attacker, defender, counterstrike=False):
+    """attacker strikes at defender. The function changes the new
+    hitpoints of the defender and returns a text String with the combat report.
+    counterstrike (boolean) indicates that this is a counterattack or not."""
+    if counterstrike:
+        t = "counterattack"
+    else:
+        t = "attack"
+    rollAtt = random.randint(1, 6)
+    rollDef = random.randint(1, 6)
+    scoreA = attacker.attack + rollAtt
+    scoreD = defender.defense + rollDef
+    if scoreA > scoreD:
+        striketext = "Sucessfull {0} !  ({1:.2f} > {2:.2f})".format(t, scoreA, scoreD)
+        damage = scoreA - scoreD
+        defender.hitpoints -= damage
+        #statistics
+        attacker.damage_dealt+= damage
+        defender.damage_received+= damage
+        striketext += "\n...doing {0:.2f} damage.".format(damage)
+    else:
+        striketext = "The {0} failed... ({1:.2f} <= {2:.2f})".format(t, scoreA, scoreD)
+    return striketext
+
+def game():
+    """the Goblin Dice Duel game main function"""
+    stinky = Goblin()
+    grunty = Goblin()
+    
+    stinky_wins=0
+    grunty_wins=0
+    
+    #save original hitpoints for next round
+    grunty_orig_hp= grunty.hitpoints
+    stinky_orig_hp= stinky.hitpoints
+    combatround = 0
+    text = ""
+
+    text += "\n --- Goblin Dice Duel ---\n\n"
+    text += compareValues(stinky, grunty)
+    text += "\n *** TURNAMENT START ***"
+
+    for x in range(1,4):
+        text += "\n\n\n ==== combat {} start ====\n\n\n".format(x)
+        #restore original hitpoints
+        grunty.hitpoints= grunty_orig_hp
+        stinky.hitpoints= stinky_orig_hp
+        while stinky.hitpoints > 0 and grunty.hitpoints > 0:
+            text += output(combatround, stinky, grunty)
+            #------------BATTLE------------
+            combatround += 1
+            if random.randint(0, 1) == 0:
+                text += "\nStinky strikes first: "
+                text += strike(stinky, grunty, False)
+                if grunty.hitpoints > 0:
+                    text += "\nCounterstrike of Grunty: "
+                    text += strike(grunty, stinky, True)
+            else:
+                text += "\nGrunty strikes first: "
+                text += strike(grunty, stinky, False)
+                if stinky.hitpoints > 0:
+                    text += "\nCounterstrike of Stinky: "
+                    text += strike(stinky, grunty, True)
+        text += output(combatround, stinky, grunty) # output of final strike
+        text += "\nGame Over"
+        if stinky.hitpoints > grunty.hitpoints:
+            stinky_wins+=1
+            text += "\nStinky wins combat {}".format(x)
+        elif grunty.hitpoints > stinky.hitpoints:
+            grunty_wins+=1
+            text += "\nGrunty wins combat {}".format(x)
+        else:
+            text += "Nobody wins ?"
+    text+="\n\n*** statistics: ***\n\n--VICTORIES    %   |Wins | Ø DD  | Ø DR -- \n    -stinky"
+    text+=" {:5.1f}% | ({}) | {:5.1f} | {:5.1f} \n    -grunty {:5.1f}% | ({}) | {:5.1f} | {:5.1f} ".format(
+             stinky_wins/(x/100),stinky_wins, stinky.damage_dealt/x,
+              stinky.damage_received/x, grunty_wins/(x/100), grunty_wins,
+              grunty.damage_dealt/x, grunty.damage_received/x)
+    text+="\n\n"+compareValues(stinky, grunty)
+    print(text)
+
+if __name__ == "__main__":
+    game()
