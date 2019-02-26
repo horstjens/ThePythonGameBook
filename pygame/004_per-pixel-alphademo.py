da5594dd24530d636ffb561d1da3b7339354e044
Brent <brent.jk.stone@gmail.com>
2019-02-26 13:09:44 -0500

    Python2 xrange -> Python3 range
    replaced xrange references with range to be compatible with Python3. Also made some minor PEP style updates.

--- pygame/004_per-pixel-alphademo.py
+++ pygame/004_per-pixel-alphademo.py
@@ -12,12 +12,9 @@ License: Do What The Fuck You Want To Public License (WTFPL)
          See http://sam.zoy.org/wtfpl/
 """
 
-####
-
 import pygame
 import os
 
-####
 
 def load_pic(name, path="data"):
 
@@ -27,49 +24,44 @@ def load_pic(name, path="data"):
     else:
         return pic.convert()
 
-####
 
 def check(x, minval=0, maxval=255):
 
     return min(maxval, max(minval, x))
 
-####
 
 def offset(len1, len2):
     """ For picture centering
     """
     return max(0, (len1 - len2) // 2)
 
-####
 
 class PeepDemo(object):
-
-
-    def __init__(self, **opts):
-
+    def __init__(self, **args):
         pygame.init()
-        self.width = opts['width']
-        self.height = opts['height']
-        self.fps = opts['fps']
+        self.width = args['width']
+        self.height = args['height']
+        self.fps = args['fps']
         self.screen = pygame.display.set_mode((self.width, self.height), pygame.DOUBLEBUF)
         self.clock = pygame.time.Clock()
         pygame.display.set_caption("Move Mouse and Scroll Mouse Wheel")
 
-        self.pic = load_pic(opts['pic'])
+        self.pic = load_pic(args['pic'])
         self.background = pygame.Surface(self.screen.get_size()).convert()
-        self.background.fill(opts['backcol'])
+        self.background.fill(args['backcol'])
 
         self.ppa_surface = pygame.Surface(self.screen.get_size(), flags=pygame.SRCALPHA)
-        self.pic_offset = offset(self.width, self.pic.get_width()),\
-                          offset(self.height, self.pic.get_height())
+        self.pic_offset = offset(self.width, self.pic.get_width()), offset(self.height, self.pic.get_height())
 
         # init stuff for circles with alpha value
         self.center = self.width // 2, self.height // 2
         self.max_radius = min(self.width, self.height)
-        self.hole_count = opts['holes']
+        self.hole_count = args['holes']
         self.calc_centers(self.center, self.center, self.hole_count)
         self.calc_rad_alphas(self.max_radius, self.hole_count)
 
+        self.rad_alphas = []
+        self.centers = []
 
     def calc_rad_alphas(self, radius, n):
         """
@@ -79,8 +71,7 @@ class PeepDemo(object):
 
         rad_step = radius // n
         alpha_step = 256 // n
-        self.rad_alphas = [(radius - i * rad_step, 255 - i*alpha_step) for i in xrange(n)]
-
+        self.rad_alphas = [(radius - i * rad_step, 255 - i*alpha_step) for i in range(n)]
 
     def calc_centers(self, center, pos, holes):
         """
@@ -93,8 +84,7 @@ class PeepDemo(object):
 
         xs = vx // holes
         ys = vy // holes
-        self.centers = [(cx + xs*i, cy + ys*i) for i in xrange(holes)]
-
+        self.centers = [(cx + xs*i, cy + ys*i) for i in range(holes)]
 
     def run(self):
         """
@@ -115,15 +105,11 @@ class PeepDemo(object):
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     # check mouse wheel
                     if event.button in (4, 5):
-                        self.hole_count = check(self.hole_count+ [-1, 1][event.button-4],
-                                                2, 64)
+                        self.hole_count = check(self.hole_count + [-1, 1][event.button-4], 2, 64)
                         self.calc_rad_alphas(self.max_radius, self.hole_count)
-                        self.calc_centers(self.center, pygame.mouse.get_pos(),
-                                          self.hole_count)
-
+                        self.calc_centers(self.center, pygame.mouse.get_pos(), self.hole_count)
             self.show()
         pygame.quit()
-
 
     def show(self):
         """
@@ -141,7 +127,6 @@ class PeepDemo(object):
         # erase alpha surface for new circles
         self.ppa_surface.fill((0, 0, 0))
 
-
     def flip(self):
         """
         Show drawing and erase
@@ -150,7 +135,6 @@ class PeepDemo(object):
         self.screen.blit(self.background, (0, 0))
         self.clock.tick(self.fps)
 
-####
 
 opts = {'width': 800,
         'height': 600,
@@ -159,8 +143,6 @@ opts = {'width': 800,
         'fontsize': 18,
         'pic': 'ente.jpg',
         'holes': 7}
-####
 
 if __name__ == "__main__":
-
     PeepDemo(**opts).run()
