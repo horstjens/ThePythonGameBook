5f146790855416aaff5e87f9e86da62a5bab05d1
Horst JENS <horstjens@gmail.com>
2019-05-02 10:20:36 +0200

    Update slowgoblins002.py

--- python/goblins/slowgoblins002.py
+++ python/goblins/slowgoblins002.py
@@ -1,6 +1,6 @@
 # part of http://ThePythonGameBook.com
 # source code: https://github.com/horstjens/ThePythonGameBook/blob/master/python/goblins/slowgoblins002.py
 
-hitpoints_stinky = 22
-hitpoints_grunty = 43
-hitpoints_grunty -= 1
+hitpointsStinky = 22
+hitpointsGrunty = 43
+hitpointsGrunty -= 1
