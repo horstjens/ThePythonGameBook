e6174f9f12309b32896d1bd8dd62fa8090d107da
Horst JENS <horstjens@gmail.com>
2019-05-02 10:21:45 +0200

    Update slowgoblins004.py

--- python/goblins/slowgoblins004.py
+++ python/goblins/slowgoblins004.py
@@ -1,13 +1,13 @@
 # part of http://ThePythonGameBook.com
 # source code: https://github.com/horstjens/ThePythonGameBook/blob/master/python/goblins/slowgoblins004.py
 
-hitpoints_stinky = 22
-hitpoints_grunty = 43
+hitpointsStinky = 22
+hitpointsGrunty = 43
 print(" --- The Goblin Dice Duel ---")
 print("game_round, Stinky, Grunty")
 for game_round in range(22):
-    print(game_round, hitpoints_stinky, hitpoints_grunty)
-    hitpoints_stinky -= 1
-    hitpoints_grunty -= 1
+    print(game_round, hitpointsStinky, hitpointsGrunty)
+    hitpointsStinky -= 1
+    hitpointsGrunty -= 1
 print()  # print an empty line
 print("Game Over")
