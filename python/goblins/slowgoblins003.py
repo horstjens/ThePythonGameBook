42b74f63dc2681f41b4d0d585c4a9921e1fe26bd
Horst JENS <horstjens@gmail.com>
2019-05-02 10:21:05 +0200

    Update slowgoblins003.py

--- python/goblins/slowgoblins003.py
+++ python/goblins/slowgoblins003.py
@@ -1,8 +1,8 @@
 # part of http://ThePythonGameBook.com
 # source code: https://github.com/horstjens/ThePythonGameBook/blob/master/python/goblins/slowgoblins003.py
 
-hitpoints_stinky = 22
-hitpoints_grunty = 43
+hitpointsStinky = 22
+hitpointsGrunty = 43
 for game_round in range(22):
-    hitpoints_stinky -= 1
-    hitpoints_grunty -= 1
+    hitpointsStinky -= 1
+    hitpointsGrunty -= 1
