4171d79cfe57d760a9fc1072ba0e1cfe34e11159
Horst JENS <horstjens@gmail.com>
2019-05-02 10:35:33 +0200

    Update slowgoblins007.py

--- python/goblins/slowgoblins007.py
+++ python/goblins/slowgoblins007.py
@@ -7,25 +7,27 @@ import random
 #later use output instead of print
 def output(c_round, hp_stinky, hp_grunty):
     """printing out three values c_round, hp_stinky and hp_grunty"""
+    # please note that hp_stinky is a local variable, only existing inside this output function
+    # hp_stinky is an PARAMETER, it's NOT the same as hitpointsStinky !
     print("{0:2d} Stinky: {1:2d} Grunty: {2:2d}".format(c_round, hp_stinky, hp_grunty))
     # no return necessary because this functions returns nothing (yet)
 
 
-hitpoints_stinky = 22
-hitpoints_grunty = 43
+hitpointsStinky = 22
+hitpointsGrunty = 43
 combat_round = 0
 print(" --- Goblin Dice Duel ---")
 print("round    hitpoints")
 
-while hitpoints_stinky > 0:
-    output(combat_round, hitpoints_stinky, hitpoints_grunty)
+while hitpointsStinky > 0:
+    output(combat_round, hitpointsStinky, hitpointsGrunty)
     combat_round += 1
-    hitpoints_grunty -= random.randint(0, 6)
-    if hitpoints_grunty <= 0:
+    hitpointsGrunty -= random.randint(0, 6)
+    if hitpointsGrunty <= 0:
         break
-    hitpoints_stinky -= random.randint(0, 6)
+    hitpointsStinky -= random.randint(0, 6)
 
-output(combat_round, hitpoints_stinky, hitpoints_grunty)  # output of final strike
+output(combat_round, hitpointsStinky, hitpointsGrunty)  # output of final strike
 print("Game Over")
 
 if hitpoints_stinky > hitpoints_grunty:
